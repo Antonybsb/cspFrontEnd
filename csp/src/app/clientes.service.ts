187fb0c6a7b1476d7b45361d4a488303cf9f663f
ANTONY DIEGO CUSTODIO DE CARVALHO <antony.carvalho@mpm.mp.br>
2022-11-16 17:48:37 -0300

    Problema criação de cliente no Banco de Dados corrigido

--- csp/src/app/clientes.service.ts
+++ csp/src/app/clientes.service.ts
@@ -8,9 +8,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ClientesService {
+
   constructor(private http: HttpClient) { }
 
-  salvar(cliente: Cliente): Observable<Cliente> {
+  salvar( cliente: Cliente ): Observable<Cliente> {
     return this.http.post<Cliente>(
       'http://localhost:8080/api/clientes',
       cliente
--- csp/src/app/clientes/clientes-form/clientes-form.component.ts
+++ csp/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,8 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-import { Cliente } from '../cliente';
-import { ClientesService } from '../../clientes.service';
+import { Cliente } from '../cliente'
+import { ClientesService } from '../../clientes.service'
 
 @Component({
   selector: 'app-clientes-form',
@@ -12,9 +12,9 @@ import { ClientesService } from '../../clientes.service';
 export class ClientesFormComponent implements OnInit {
 
   //Para receber os dados do cliente para o template
-  cliente!: Cliente;
+  cliente: Cliente;
   success: boolean = false;
-  errors!: String[];
+  errors?: String[];
 
   constructor(private service: ClientesService) {
     this.cliente = new Cliente();
@@ -25,15 +25,15 @@ export class ClientesFormComponent implements OnInit {
 
   onSubmit() {
     this.service
-      .salvar(this.cliente)
+     .salvar(this.cliente)
       .subscribe(response => {
-        this.success = true;
-        this.errors = [];
-        this.cliente = response;
-      }, errorResponse => {
-        this.success = false;
+      this.success = true;
+      } , errorResponse => {
         this.errors = errorResponse.error.errors;
+    }
+    )
+
       }
-      )
-  }
+
+
 }
